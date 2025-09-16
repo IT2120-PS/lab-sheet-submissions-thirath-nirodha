4e45535ed79be84b256e0321247f99e6e6195957
thirath-nirodha <84507606+thirath-nirodha@users.noreply.github.com>
2025-09-17 00:03:00 +0530

    Add files via upload

---
+++ Lab Sheet 07/IT24104295_Lab7.R
@@ -0,0 +1,5 @@
+setwd('C:\\Users\\USER\\Desktop\\IT24103847[PS_Lab7]')
+punif(25,min = 0,max=40,lower.tail = TRUE)-punif(10,min = 0,max=40,lower.tail = TRUE)
+pexp(2,rate=1/3,lower.tail =TRUE)
+pnorm(130,mean=100,sd=15,lower.tail = FALSE)
+qnorm(0.95,mean=100,sd=15,lower.tail = TRUE)
